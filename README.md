3162ea01ab66feed1c38c6c4521d0fbd970426c4
Michael Greene <michael.greene.27@gmail.com>
2018-10-12 18:33:28 -0500

    added sample sequence files for formatting example; mandated that all input must be .txt files

--- README.md
+++ README.md
@@ -3,21 +3,23 @@ The task this accomplishes can be described as inserting gaps into two sequences
 
 To accomplish this task, the Needleman-Wunsch algorithm is implemented, as well as an anchored version. Anchored Needleman-Wunsch assumes some regions ('anchors') are known to match in both sequences, and the algorithm takes as input the start and end positions of each anchor as well as the two sequences to be aligned. The task of sequence alignment then becomes aligning the regions between anchors and building up the final alignment, saving time.
 
-If desired, this program will also permute both sequences, perform alignment and scoring, and repeat 10,000 times. The resulting scores are then made into a histogram and saved to a PDF file.
+If desired, this program will also permute both sequences, perform alignment & scoring, and repeat 10,000 times. The resulting 1,000 scores are then made into a histogram and saved to a PDF file. This supports the claim that the unpermuted alignment is optimal.
 
 ## INPUT notes:
-To run this program, the sequence files MUST be in the same folder as the program file 'HW2.py', files CANNOT be opened by passing their path to the program. Input files must be readable as a CSV file.
+To run this program, the sequence files MUST be in the same folder as the program file 'SequenceAlignment.py', files CANNOT be opened by passing their path to the program. Input files must .txt format, due to how they are parsed.
 
 ## OUTPUT notes:
 Program always outputs a text file 'out.txt' containing the new sequences (with gaps inserted) and their alignment score. If a match file is passed as well, the program runs anchored Needleman-Wunsch. The program will also ask the user before repeatedly permuting and realigning the sequences, ultimately creating a histogram of alignment scores. If this last step is executed, the output of this process is the file 'AlignmentScoreHistogram.pdf'.
 
-***********************************************************************************
-## USAGE:
-python HW2.py *name of reference FASTA file* *query FASTA file* [*match txt file*]
-***********************************************************************************
+***********************************************************************************************
 
-Ex: python HW2.py Human_HOX.fa Fly_HOX.fa Match_HOX.txt
+## USAGE:
+python SequenceAlignment.py *name of reference txt file* *query txt file* [*match txt file*]
+
+***********************************************************************************************
+
+Ex: python SequenceAlignment.py Human_HOX.txt Fly_HOX.txt Match_HOX.txt
 In this example the Human_HOX file becomes the reference sequence and Fly_HOX becomes the query. Match_HOX is used to run anchored needleman-wunsch for the two sequences.
 
-Also acceptable: python HW2.py Human_HOX.fa Fly_HOX.fa
+Also acceptable: python SequenceAlignment.py Human_PAX.txt Fly_PAX.txt
 In this example, the non-anchored needleman-wunsch algorithm will be run for the two given files, with Human_HOX and Fly_HOX used as reference and query, respectively.
--- SequenceAlignment.py
+++ SequenceAlignment.py
@@ -27,6 +27,7 @@ def init_scoring_matrix(reflen, qlen):
 
 #assume ref and query are strings
 def needleman_wunsch(ref, query):
+    
     reflen = len(ref)
     qlen = len(query)
     
@@ -108,111 +109,112 @@ def needleman_wunsch(ref, query):
 
 #assume match is a 2d array size n*4
 def anchored_needleman_wunsch(ref, query, match=None):
-	print("beginning initial alignment")
+    print("beginning initial alignment")
 
-	#if not anchored, do regular needleman-wunsch
-	if not match: return(needleman_wunsch(ref, query))
+    #if not anchored, do regular needleman-wunsch
+    if type(match) == None: return(needleman_wunsch(ref, query))
 
-	roffset = 0 #keeps track of the offset in the human sequence
-	qoffset = 0 #how much of the fly sequence has been processed
+    roffset = 0 #keeps track of the offset in the human sequence
+    qoffset = 0 #how much of the fly sequence has been processed
 
-	#return values- built as program execution proceeds
-	refstr = ""
-	qstr = ""
-	alignscore = 0
-	    
-	#for each anchor: align the area, and the area between the last anchor and the current one
-	#dont let aligned mismatch areas be replicated and aligned twice (use a string offset)
-	for i in range(len(match)):        
-	    #the first two columns of match are for the start and end positions of the human (reference) sequence
-	    #last two are for the start and end position of the fly (query) sequence
-	    rlow = int(match[i][0])
-	    rhigh = int(match[i][1])
-	    qlow = int(match[i][2])
-	    qhigh = int(match[i][3])
-	        
-	    before = needleman_wunsch(ref[roffset:rlow], query[qoffset:qlow]) #mismatch region between previous and current anchor
-	    anchor = needleman_wunsch(ref[rlow:rhigh], query[qlow:qhigh]) 
-	    #must align anchors because they arent guaranteed to be 100% match (no harm if they were 100% match)
-	        
-	    refstr += (before[0] + anchor[0]) #order is important, this is not a symmetric operation!
-	    qstr += (before[1] + anchor[1])
-	    alignscore += (before[2] + anchor[2]) #this one is symmetric
-	        
-	    #each iteration aligns the anchor and the region preceding it (succeeding the previous anchor)
-	    #this means the region following the last anchor isnt implicitly aligned!
-	    #check for last anchor
-	    if i == len(match)-1:
-	        #explicitly align the region following the last anchor
-	        after = needleman_wunsch(ref[rhigh:], query[qhigh:])
-	        refstr += after[0]
-	        qstr += after[1]
-	        alignscore += after[2]
-	            
-	    #increment the reference and query offset- otherwise the same region will be aligned several times
-	    roffset = rhigh #high is the index of the transition from anchor into the next mismatch region
-	    qoffset = qhigh
-	       
-	return [refstr, qstr, alignscore]	
+    #return values- built as program execution proceeds
+    refstr = ""
+    qstr = ""
+    alignscore = 0
+        
+    #for each anchor: align the area, and the area between the last anchor and the current one
+    #dont let aligned mismatch areas be replicated and aligned twice (use a string offset)
+    for i in range(len(match)):        
+        #the first two columns of match are for the start and end positions of the human (reference) sequence
+        #last two are for the start and end position of the fly (query) sequence
+        rlow = int(match[i][0])
+        rhigh = int(match[i][1])
+        qlow = int(match[i][2])
+        qhigh = int(match[i][3])
+            
+        before = needleman_wunsch(ref[roffset:rlow], query[qoffset:qlow]) #mismatch region between previous and current anchor
+        anchor = needleman_wunsch(ref[rlow:rhigh], query[qlow:qhigh]) 
+        #must align anchors because they arent guaranteed to be 100% match (no harm if they were 100% match)
+            
+        refstr += (before[0] + anchor[0]) #order is important, this is not a symmetric operation!
+        qstr += (before[1] + anchor[1])
+        alignscore += (before[2] + anchor[2]) #this one is symmetric
+            
+        #each iteration aligns the anchor and the region preceding it (succeeding the previous anchor)
+        #this means the region following the last anchor isnt implicitly aligned!
+        #check for last anchor
+        if i == len(match)-1:
+            #explicitly align the region following the last anchor
+            after = needleman_wunsch(ref[rhigh:], query[qhigh:])
+            refstr += after[0]
+            qstr += after[1]
+            alignscore += after[2]
+                
+        #increment the reference and query offset- otherwise the same region will be aligned several times
+        roffset = rhigh #high is the index of the transition from anchor into the next mismatch region
+        qoffset = qhigh
+           
+    return [refstr, qstr, alignscore]   
 
 def permute_and_graph(ref, query):
-	print("beginning permutations")
+    print("beginning permutations")
 
-	scores = [needleman_wunsch(ref, query)[2]] #add the unpermuted score first
+    scores = [needleman_wunsch(ref, query)[2]] #add the unpermuted score first
 
-	i = 0
-	while i<10000:
-	    i += 1 #because I would've forgotten if this were last
-	    refperm = "".join(np.random.permutation(list(ref))) #permutation operates on the list of chars represented by ref
-	    queryperm = "".join(np.random.permutation(list(query))) #"".join() joins into a string each char in the array returned by permutation
+    i = 0
+    while i<1000:
+        i += 1 #because I would've forgotten if this were last
+        refperm = "".join(np.random.permutation(list(ref))) #permutation operates on the list of chars represented by ref
+        queryperm = "".join(np.random.permutation(list(query))) #"".join() joins into a string each char in the array returned by permutation
 
-	    #return from needleman-wunsch is [refstring, querystring, score]
-	    #dont use anchored needleman-wunsch because the matches are meaningless after permuting sequences
-	    scores.append(needleman_wunsch(refperm, queryperm)[2])
+        #return from needleman-wunsch is [refstring, querystring, score]
+        #dont use anchored needleman-wunsch because the matches are meaningless after permuting sequences
+        scores.append(needleman_wunsch(refperm, queryperm)[2])
 
-	figure = plt.figure()
-	plt.hist(scores, bins=100)
-	plt.axvline(scores[0], linestyle="dashed", label="unpermuted score") #add a vertical line to show the unpermuted score
-	plt.title("frequency of alignment scores for random permutations of human and fruit fly hox genes")
-	plt.xlabel("alignment score")
-	plt.ylabel("frequency")
-	plt.legend()
-	plt.show()
-	figure.savefig("AlignmentScoreHistogram.pdf")
+    figure = plt.figure()
+    plt.hist(scores, bins=100)
+    plt.axvline(scores[0], linestyle="dashed", label="unpermuted score") #add a vertical line to show the unpermuted score
+    plt.xlabel("alignment score")
+    plt.ylabel("frequency")
+    plt.legend()
+    plt.show()
+    figure.savefig("AlignmentScoreHistogram.pdf")
 
 
 if __name__ == '__main__':
-	#invoke this program as python SequenceAlignment.py <reference> <query> [<match>]
-	#sys.argv[0] = script name
-	reffile = sys.argv[1]
-	queryfile = sys.argv[2]
-	matchfile = None if len(sys.argv) <= 3 else sys.argv[3]
-		
-	try: refstr = pd.read_csv(reffile)
-	except: print("failed to open reference file")
-	refstr = refstr[refstr.columns[0]] #specific to how pandas reads a csv into a data frame
+    #invoke this program as python SequenceAlignment.py <reference> <query> [<match>]
+    #sys.argv[0] is the script name (SequenceAlignment.py)
+    reffile = sys.argv[1]
+    queryfile = sys.argv[2]
+    matchfile = None if len(sys.argv) <= 3 else sys.argv[3]
+        
+    try:
+        refstr = str(pd.read_csv(reffile).values[0]) #values returns an array of data entries, index 0 to get the (presumed) only entry
+        print(type(refstr))
+    except: print("\nfailed to open reference file\n")
+        
 
-	try: querystr = pd.read_csv(queryfile)
-	except: print("failed to open query file")
-	querystr = querystr[querystr.columns[0]]
+    try:
+        querystr = str(pd.read_csv(queryfile).values[0])
+        print(type(querystr))
+    except: print("\nfailed to open query file\n")
 
-	try: 
-		match_unprocessed = open(matchfile, "r")
-		match_tmp = match_unprocessed.read().trim().split("\n") #must be open before read (greedy exec)
-		matchv = list(map(lambda group:
-			group.split("\t"), match_tmp))
-	except: #exception caught when match file is not supplied
-		matchv = None
+    try:
+        matchv = pd.read_csv(matchfile).values
+    except: #exception caught when match file is not supplied (equals null)
+        matchv = None
 
-	alignment = anchored_needleman_wunsch(refstr, querystr, matchv)
+    #alignment = anchored_needleman_wunsch(refstr, querystr, matchv)
+    alignment = anchored_needleman_wunsch(refstr, querystr, matchv)
 
-	output = open("out.txt", "w+")
-	output.write("Refrence: \n\t" + str(alignment[0]))
-	output.write("\n\nQuery: \n\t" + str(alignment[1]))
-	output.write("\nAlignment score: " + str(alignment[2]))
-	output.close() #if performing permutations times out (it takes a while to perform) then output from NW will still be output to text
+    outfile = open("out.txt", "w+")
+    outfile.write("Refrence: \n\t" + str(alignment[0]))
+    outfile.write("\n\nQuery: \n\t" + str(alignment[1]))
+    outfile.write("\nAlignment score: " + str(alignment[2]))
+    outfile.close() #if performing permutations times out (it takes a while to perform) then output from NW will still be output to text
 
-	if input("Permute and graph? type Y/N: ") == "Y": permute_and_graph(refstr, querystr)
+    if input("Permute and graph? type Y/N: ") == "Y": permute_and_graph(refstr, querystr)
 
-	print("Execution finished successfully")
+    print("Execution finished successfully")
+
 
